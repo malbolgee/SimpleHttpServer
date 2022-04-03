c21a51cb72abb61942e7ce111fc0f4557ddde7a5
Malbolge <victor.gomes@icomp.ufam.edu.br>
2022-04-03 09:55:25 -0400

    Genesis: Initial commit
    Creates the HttpServer driver class and the pom file.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

---
+++ src/main/java/com/malbolge/httpserver/HttpServer.java
@@ -0,0 +1,11 @@
+package com.malbolge.httpserver;
+
+/**
+ * Driver class for the Http Server
+ */
+public class HttpServer {
+	public static void main(String[] args) {
+		System.out.println("Server starting...");
+	}
+
+}
